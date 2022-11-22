2ad5aeed8ec08fe19c171317ddb663b3b3516587
u2cc <cheng.james.chen@gmail.com>
2022-11-22 15:32:24 -0500

    Initial working commit

---
+++ src/main/java/com/weblogic/rest/HelloResource.java
@@ -0,0 +1,16 @@
+package com.weblogic.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!Great OK!";
+    }
+}
+
